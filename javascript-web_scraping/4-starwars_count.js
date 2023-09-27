fac0f264da9b1fc17236e9984a024958b8b6a299
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-27 12:40:58 +0200

    filtering out ID 18

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const apiUrl = process.argv[2];
-const characterId = 18;
+const characterId = '18';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
+    process.exit(1);
   }
 
-  const films = JSON.parse(body).results;
-  const matchingMovies = films.filter(film =>
+  const filmsData = JSON.parse(body).results;
+  const matchingMovies = filmsData.filter(film =>
     film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
   );
 
