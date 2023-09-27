065c6731fc15d1626060468dea5939c0cc52b2e0
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-27 12:55:14 +0200

    saving webpage content in file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,22 +2,7 @@
 
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
-const filePath = process.argv[3];
+const fileName = process.argv[3];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-    if (writeError) {
-      console.error(writeError);
-      process.exit(1);
-    }
-
-    console.log(`File saved as ${filePath}`);
-  });
-});
+request.get(url).pipe(fs.createWriteStream(fileName));
