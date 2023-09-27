f800326c70f2ec2ee506e496861f5cc369efc056
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-09-27 11:44:47 +0200

    writing txt in file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,10 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, contentToWrite, 'utf-8', function (err) {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`Content written to ${filePath}`);
   }
 });
